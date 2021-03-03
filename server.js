33b6128b2431ed63aca3eb22667c55654ba01309
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-03 16:36:44 +0700

    Add cors to header

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const app = express()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
