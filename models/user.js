4d5f02e8a9db157e1ae726ca57a2e9568b859df0
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-09 15:34:56 +0700

    edit

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    name: String,
     gender: Boolean,
     birthday: Date,
     isNewUser: false
