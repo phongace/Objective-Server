428e86ccdfd038e3ffaeb3077f8f339d89630b71
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-16 17:39:32 +0700

    edit

--- routes/target-api.js
+++ routes/target-api.js
@@ -23,6 +23,7 @@ router.post('/', checkAuth, (req, res) => {
       })
     } else {
       const target = new Target({
+        _id: 'dasdsdasd',
         userId,
         title,
         description,
