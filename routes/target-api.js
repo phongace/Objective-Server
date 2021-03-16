d8af0c709117aac73b6cf25fe33027b6fecb0cff
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-16 14:37:30 +0700

    id auto increment

--- routes/target-api.js
+++ routes/target-api.js
@@ -38,15 +38,7 @@ router.post('/', checkAuth, (req, res) => {
           res.json({
             status: 'SUCCESS',
             message: 'Target created!',
-            data: {
-              id: result._id,
-              userId: result.userId,
-              title: result.title,
-              description: result.description,
-              time: result.time,
-              isDone: result.isDone,
-              subTask: result.subTask
-            }
+            data: result
           })
         })
         .catch(error => {
