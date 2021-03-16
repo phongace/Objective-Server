e59ff5b458faf9d22a15e37886d47e366b51dddc
Phong Nguyen Hoang <phongnh@timebird.org>
2021-03-16 10:06:07 +0700

    api create target

--- routes/target-api.js
+++ routes/target-api.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const router = express.Router()
 const { checkAuth } = require('../middleware/checkAuth')
+const Target = require('../models/target')
 
 router.post('/', checkAuth, (req, res) => {
-  let { title, description, time } = req.body
+  var { title, description, time } = req.body
   title = title.trim()
   description = description.trim()
   time = time.trim()
 
-  if (title === '' || description === '' || time === '') {
-    res.json({
-      status: 'FAILED',
-      message: 'Empty input fields!'
-    })
-  } else {
+  if (req.headers && req.headers.authorization) {
+    var decoded = jwt.verify(
+      req.headers['authorization'].split(' ')[1],
+      process.env.ACCESS_TOKEN_SECRET
+    )
+    var userId = decoded.id
+    if (title === '' || description === '' || time === '') {
+      res.json({
+        status: 'FAILED',
+        message: 'Empty input fields!'
+      })
+    } else {
+      const target = new Target()
+      target.id = 1
+      target.userId = userId
+      target.title = title
+      target.description = description
+      target.time = time
+      target.isDone = false
+      target
+        .save()
+        .then(result => {
+          res.json({ status: 'SUCCESS', message: 'Target created!', result })
+        })
+        .catch(error => {
+          res.json({
+            status: 'FAILED',
+            error
+          })
+        })
+    }
   }
 })
